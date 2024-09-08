926b70bf4ceb7e595733099c315f276beacfed2e
Amanatiadis-Panagiotis <amanatiadispan@gmail.com>
2024-09-08 16:20:31 +0300

    add the solutions from set02.

---
+++ 2o Semester/Data Structures/Stacks/a6f2.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define StackLimit 50   // το όριο μεγέθους της στοίβας, ενδεικτικά ίσο με 50
+
+
+typedef int StackElementType;   // ο τύπος των στοιχείων  της στοίβας
+                                //ενδεικτικά τύπος int
+typedef struct  {
+    int Top;
+    StackElementType Element[StackLimit];
+} StackType;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+void CreateStack(StackType *Stack);
+boolean EmptyStack(StackType Stack);
+boolean FullStack(StackType Stack);
+void Push(StackType *Stack, StackElementType Item);
+void Pop(StackType *Stack, StackElementType *Item);
+
+int GetTopElementA(StackType *Stack);
+int GetTopElementB(StackType *Stack);
+
+int main()
+{
+    int i,top1,top2;
+    StackType Stack;
+
+
+    CreateStack(&Stack);
+
+    for(i=1; i<=99; i=i+2)
+    {
+        Push(&Stack,i);
+    }
+
+    top1=GetTopElementA(&Stack);
+    printf("TOPA = %d\n",top1);
+
+    top2=GetTopElementB(&Stack);
+    printf("TOPB = %d\n",top2);
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+
+int GetTopElementA(StackType *Stack)
+{
+    int top;
+
+     if(!EmptyStack(*Stack))
+     {
+         return (Stack->Element[Stack->Top]);
+
+     }
+
+
+
+}
+
+int GetTopElementB(StackType *Stack)
+{
+    return Stack->Element[Stack->Top];
+}
+
+
+void CreateStack(StackType *Stack)
+/* Λειτουργία: Δημιουργεί μια κενή στοίβα.
+   Επιστρέφει: Κενή Στοίβα.*
+*/
+{
+    Stack -> Top = -1;
+   // (*Stack).Top = -1;
+}
+
+boolean EmptyStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι κενή.
+   Επιστρέφει: True αν η Stack είναι κενή, False διαφορετικά
+*/
+{
+    return (Stack.Top == -1);
+}
+
+boolean FullStack(StackType Stack)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Ελέγχει αν η στοίβα Stack είναι γεμάτη.
+   Επιστρέφει: True αν η Stack είναι γεμάτη, False διαφορετικά
+*/
+{
+    return (Stack.Top == (StackLimit - 1));
+}
+
+void Push(StackType *Stack, StackElementType Item)
+/* Δέχεται: Μια στοίβα Stack και ένα στοιχείο Item.
+   Λειτουργία: Εισάγει το στοιχείο Item στην στοίβα Stack αν η Stack δεν είναι γεμάτη.
+   Επιστρέφει: Την τροποποιημένη Stack.
+   Έξοδος: Μήνυμα γεμάτης στοίβας, αν η στοίβα Stack είναι γεμάτη
+*/
+{
+    if (!FullStack(*Stack)) {
+        Stack -> Top++;
+        Stack -> Element[Stack -> Top] = Item;
+    } else
+        printf("Full Stack...");
+}
+
+void Pop(StackType *Stack, StackElementType *Item)
+/* Δέχεται: Μια στοίβα Stack.
+   Λειτουργία: Διαγράφει το στοιχείο Item από την κορυφή της Στοίβας αν η Στοίβα δεν είναι κενή.
+   Επιστρέφει: Το στοιχείο Item και την τροποποιημένη Stack.
+   Έξοδος:  Μήνυμα κενής στοίβας αν η Stack είναι κενή
+*/
+{
+    if (!EmptyStack(*Stack)) {
+        *Item = Stack -> Element[Stack -> Top];
+        Stack -> Top--;
+    } else
+        printf("Empty Stack...");
+}
+
+
